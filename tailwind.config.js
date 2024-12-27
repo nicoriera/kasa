c873af82c6e78f8085a7c00f64231b7f9c99b358
nicolas riera <nicolariera@gmail.com>
2024-12-27 16:06:01 +0100

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
