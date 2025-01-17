a5554b68959a40b20934f982443a9de886f78aa1
nicolas riera <nicolariera@gmail.com>
2025-01-17 10:16:52 +0100

    implement element for a propos page and logic for open dropdown and responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,21 @@
 import "../styles/banner.scss";
+import PropTypes from "prop-types";
 
-function Banner() {
+function Banner({ backgroundImage, text }) {
   return (
-    <div className="banner">
+    <div
+      className="banner"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div className="overlay"></div>
-      <p className="banner-text">Chez vous, partout et ailleurs</p>
+      <p className="banner-text">{text}</p>
     </div>
   );
 }
+
+Banner.propTypes = {
+  backgroundImage: PropTypes.string.isRequired,
+  text: PropTypes.string,
+};
 
 export { Banner };
--- src/pages/APropos.jsx
+++ src/pages/APropos.jsx
@@ -1,5 +1,41 @@
+import { Dropdown } from "../components/Dropdown";
+import { Banner } from "../components/Banner";
+import bannerImage from "../assets/banner-image-a-propos.jpeg";
+
 function APropos() {
-  return <div>APropos</div>;
+  const size = {
+    large: "large",
+    medium: "medium",
+    small: "small",
+  };
+
+  return (
+    <div className="a-propos">
+      <Banner backgroundImage={bannerImage} />
+      <div className="dropdown-container">
+        <Dropdown
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          size={size.large}
+        />
+        <Dropdown
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          size={size.large}
+        />
+        <Dropdown
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          size={size.large}
+        />
+        <Dropdown
+          title="Sécurité"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          size={size.large}
+        />
+      </div>
+    </div>
+  );
 }
 
 export { APropos };
