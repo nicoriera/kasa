b888fdd5eed9c8fe8bcf1245fae3f54dd16ca6e9
nicolas riera <nicolariera@gmail.com>
2025-01-17 10:51:50 +0100

    implement page 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import "../styles/404.scss";
+
 function NotFound() {
-  return <div>404</div>;
+  return (
+    <div className="not-found">
+      <h1 className="not-found-title">404</h1>
+      <p className="not-found-text">
+        Oups! La page que vous demandez n&apos;existe pas.
+      </p>
+      <Link to="/" className="not-found-link">
+        Retourner sur la page d&apos;accueil
+      </Link>
+    </div>
+  );
 }
 
 export { NotFound };
