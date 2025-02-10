c2a36d73acd2d22e6d9ab15851f494b9780f9edf
nicolas riera <nicolariera@gmail.com>
2025-02-10 23:44:36 +0100

    fix routing for local and prod

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/kasa/",
+  base: mode === "production" ? "/kasa/" : "/",
   server: {
     proxy: {
       "/api-images": {
@@ -14,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
